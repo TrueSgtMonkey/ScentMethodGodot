e85c9ab8e15834cd9812d64d0b742296f3798f18
TrueSgtMonkey <codythompsonwp4@gmail.com>
2026-08-19 17:57:47 -0700

    NPC Idle with RayShot into Chase State
    * NPC now has an idle state in which it moves in random directions
    within 360 degrees.
    * NPC also has vision checks to see if it can see the player
    * If the NPC spots the player, goes into chase state
    * If the NPC hasn't seen the player for long enough during chase,
    then it goes back into idle state.
    * Player also has noclip capabilities now.

--- breadcrumb_example/global_control.gd
+++ breadcrumb_example/global_control.gd
@@ -2,6 +2,9 @@ extends Node
 
 var player_ref: CharacterBody3D = null
 
+func _ready():
+	randomize()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("quit_game"):
 		get_tree().quit()
